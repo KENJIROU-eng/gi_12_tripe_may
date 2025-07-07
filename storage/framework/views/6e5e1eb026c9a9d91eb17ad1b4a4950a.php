1e853bb9574377a8c6fcd3323753f6f575484edf
KENJIROU-eng <Atene1723@gmail.com>
2025-07-07 17:48:18 +0900

    update_20250707

--- storage/framework/views/6e5e1eb026c9a9d91eb17ad1b4a4950a.php
+++
@@ -1,8 +0,0 @@
-<div id="notify-box" class="fixed bottom-16 right-4 bg-white p-4 rounded shadow max-w-sm z-50">
-    <p>Do you wanna receive the notifications?</p>
-    <div class="flex gap-2 mt-4">
-        <button onclick="enableNotification()" class="bg-blue-500 text-white flex-1 py-1 rounded">Yes</button>
-        <button onclick="dismissNotification()" class="bg-gray-300 text-gray-800 flex-1 py-1 rounded">Cancel</button>
-    </div>
-</div>
-<?php /**PATH C:\Users\USER\Desktop\gi_12_tripe_may\resources\views/livewire/notification-toggle.blade.php ENDPATH**/ ?>
--- storage/framework/views/85b3437f535019bb6b5fb5ec827f8580.php
+++
@@ -1,9 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<<<<<<<< HEAD:storage/framework/views/85b3437f535019bb6b5fb5ec827f8580.php
-<?php /**PATH C:\Users\Tamak\Desktop\gi_12_tripe_may\resources\views/components/primary-button.blade.php ENDPATH**/ ?>
-========
-<?php /**PATH C:\Users\USER\Desktop\gi_12_tripe_may\resources\views/components/primary-button.blade.php ENDPATH**/ ?>
->>>>>>>> 6df93b8a75dd54c8cbc6cfa7579d8c9215562e33:storage/framework/views/5aa62b14cf6d6f784dc67cc87ff9f2e4.php
